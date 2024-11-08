6d0200aebf4e9d92873dd1284c00e63d9ff9e864
Jose Luu <jose.luu@gmail.com>
2024-11-08 15:59:45 +0100

    add install and environment setup files

---
+++ install.sh
@@ -0,0 +1,6 @@
+npm install @truffle/hdwallet-provider
+npm install dotenv web3@1.7.4
+npm install @openzeppelin/contracts@4.9.0
+npm install truffle-abi
+npm install truffle@5.6.0
+npm install truffle-contract-size
