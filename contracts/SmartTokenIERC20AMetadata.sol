a1df17fbb587f292614775ec2b5b2b375af32ce5
GRIFFAULTV <84021819+vgr942@users.noreply.github.com>
2024-12-13 19:04:56 +0100

    rename SmartTokenIERC20AMetadata.sol to ISmartTokenERC20AMetadata.sol

--- contracts/SmartTokenIERC20AMetadata.sol
+++ contracts/SmartTokenIERC20AMetadata.sol
@@ -1,11 +1,9 @@
 // SPDX-License-Identifier: MIT
-// documentation here: https://docs.openzeppelin.com/contracts/4.x/api/token/erc20#IERC20Metadata
-
 pragma solidity ^0.8.17;
 
-import "./SmartTokenIERC20A.sol";
+import {ISmartTokenERC20A} from "./ISmartTokenERC20A.sol";
 
-interface SmartTokenIERC20AMetadata is SmartTokenIERC20A {
+interface ISmartTokenERC20AMetadata is ISmartTokenERC20A {
 
     function get_name() external view returns (string memory);
 
