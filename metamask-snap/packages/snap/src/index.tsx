3ee987b40457a8dc7f2f36e58d471421de588f9b
hmzaakun <hamza.mekhaneg@icloud.com>
2025-02-07 10:58:45 +0100

    feat: enhance SmartDirectory integration with custom client creation and chain support

--- metamask-snap/packages/snap/src/index.tsx
+++ metamask-snap/packages/snap/src/index.tsx
@@ -1,16 +1,32 @@
 import type { OnTransactionHandler } from "@metamask/snaps-sdk";
 import { Box, Heading, Text, Bold, Address } from "@metamask/snaps-sdk/jsx";
 
-import { createPublicClient, http } from 'viem';
-import { polygonAmoy } from 'viem/chains';
+import { Chain, createPublicClient, http } from 'viem';
+import { polygonAmoy, sepolia } from 'viem/chains';
 import { abi } from './abi';
 
-const client = createPublicClient({
-  chain: polygonAmoy,
-  transport: http("https://polygon-amoy.drpc.org"),
-});
+const smartDirectoryConfig: [number, Chain, string, `0x${string}`][] = [
+  [polygonAmoy.id, polygonAmoy, "https://polygon-amoy.drpc.org", "0x88CBa1e32db10CE775210C80A39F407EAA982E0D"],
+  [sepolia.id, sepolia, "https://sepolia.drpc.org", "0x88CBa1e32db10CE775210C80A39F407EAA982E0D"],
+];
 
-const getReferenceLastStatus = async (SMDirAddress: `0x${string}`, referenceAddress: `0x${string}`) => {
+const createCustomClient = (chainId: number) => {
+  const [id, clientObject, url, SMDirAddress] = smartDirectoryConfig.find(([configId]) => configId = chainId) ?? [];
+  try {
+    if (!id) {
+      throw new Error("Invalid chainId");
+    }
+    return createPublicClient({
+      chain: clientObject,
+      transport: http(url),
+    });
+  } catch (error) {
+    return String(error);
+  }
+}
+
+const getReferenceLastStatus = async (SMDirAddress: `0x${string}`, referenceAddress: `0x${string}`, customClient: any) => {
+  const client = customClient;
   var referenceStatus: string | null = null;
   try {
     const ReferenceStatuslastIndex = await client.readContract({
@@ -36,20 +52,21 @@ const getReferenceLastStatus = async (SMDirAddress: `0x${string}`, referenceAddr
   return referenceStatus;
 }
 
-const getRegistrantUri = async (SMDirAddress: `0x${string}`, referenceAddress: `0x${string}`) => {
+const getRegistrantUri = async (SMDirAddress: `0x${string}`, referenceAddress: `0x${string}`, customClient: any) => {
+  const client = customClient;
   var registrantAddress: `0x${string}` | null = null;
   try {
-      const registrantUriResult = await client.readContract({
-        address: SMDirAddress,
-        abi,
-        functionName: 'getReference',
-        args: [referenceAddress],
-      });
-      registrantAddress = registrantUriResult[0] as `0x${string}` ?? null;
-    } catch (error) {
-      console.error(error);
-      return null;
-    }
+    const registrantUriResult = await client.readContract({
+      address: SMDirAddress,
+      abi,
+      functionName: 'getReference',
+      args: [referenceAddress],
+    });
+    registrantAddress = registrantUriResult[0] as `0x${string}` ?? null;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
 
   var registrantUri: string | null = null;
   try {
@@ -71,21 +88,21 @@ const getRegistrantUri = async (SMDirAddress: `0x${string}`, referenceAddress: `
 
 
 // Handle outgoing transactions.
-export const onTransaction: OnTransactionHandler = async ({ transaction }) => {
+export const onTransaction: OnTransactionHandler = async ({ transaction, chainId }) => {
   const SMdirAddress = "0x88CBa1e32db10CE775210C80A39F407EAA982E0D";
-  const referenceInfo = await getReferenceLastStatus(SMdirAddress, transaction.to as `0x${string}`)
-  const registrantUri = await getRegistrantUri(SMdirAddress, transaction.to as `0x${string}`)
-  console.log(referenceInfo)
-  
+  const customClient = createCustomClient(chainId as unknown as number);
+  const referenceInfo = await getReferenceLastStatus(SMdirAddress, transaction.to as `0x${string}`, customClient)
+  const registrantUri = await getRegistrantUri(SMdirAddress, transaction.to as `0x${string}`, customClient)
+
   return {
     content: (
       <Box>
         <Heading>{referenceInfo !== null ? "✅🔐" : "⛔️⛔️⛔️ Unknown Smartcontract ⛔️⛔️⛔️"}</Heading>
         <Text>
 
-          You are interacting with <Bold>{transaction.to as `0x${string}`}</Bold> {referenceInfo? "which is part of the SmartDirectory.": ""}
-            {referenceInfo !== null ? " The reference status is: " + JSON.stringify(referenceInfo) : " This address is not part of the smartdirectory."}
-            {registrantUri !== null ? " The registrant URI is : " + registrantUri : ""}
+          You are interacting with <Bold>{transaction.to as `0x${string}`}</Bold> {referenceInfo ? "which is part of the SmartDirectory." : ""}
+          {referenceInfo !== null ? " The reference status is: " + JSON.stringify(referenceInfo) : " This address is not part of the smartdirectory."}
+          {registrantUri !== null ? " The registrant URI is : " + registrantUri : ""}
         </Text>
       </Box>
     ),
