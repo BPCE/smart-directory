79e0f72aae786d00e3c56c60fb0c365fb11cb7d1
José LUU <jose.luu@gmail.com>
2025-09-11 17:02:18 +0200

    remove extraneous functions

--- contracts/ISmartDirectoryERC.sol
+++ contracts/ISmartDirectoryERC.sol
@@ -54,14 +54,12 @@ function getActivationCode() external view returns(ActivationCode);
 
     function getReference(address _referenceAddress) external view returns (
         address registrantAddress,
-        uint256 registrantIndex,
         address referenceAddress,
-        string memory projectId,
+        string memory referenceDescription,
         string memory referenceType,
         string memory referenceVersion,
         string memory status,
         uint256 timeStamp);
-
 
 
     function getReferenceStatusAtIndex(address _referenceAddress, uint256 _index) external view returns
@@ -78,26 +76,12 @@ function getActivationCode() external view returns(ActivationCode);
 
     function getRegistrantUri(address _registrantAddress) external view returns (string memory);
 
-    function getRegistrantAtIndex(uint256 _registrantIndex) external view returns (address registrantAddress,
-        string memory registrantUri);
-
-    function getRegistrantIndex(address _registrantAddress) external view returns(uint256);
-
-    function getRegistrantLastIndex() external view returns (uint256);
-
-    function getRegistrantReferencesCount(address _registrantAddress) external view returns (uint256);
-
     function getDisabledRegistrants() external view returns (address[] memory disabledRegistrantsList);
 
 //SMART DIRECTORY UTILITY FUNCTIONS
 
     function version() external pure returns (string memory);
 
-    function getParent1() external view returns(address);
-
-    function getParent2() external view returns(address);
-
-    function getContractType() external pure returns(uint8);
 
 
 }
